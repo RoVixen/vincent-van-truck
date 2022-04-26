8ef0edd06a9e055c7151576f47d300675fe901e2
RoVixen <griffertyuiop@gmail.com>
2022-04-26 16:38:13 -0400

    shit, fetchign files is not easy

--- checkforuploads.js
+++ checkforuploads.js
@@ -1,3 +1,5 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const Discord = require('discord.js');
 const fse=require("fs-extra")
 
@@ -11,10 +13,27 @@ const { handleFileErr } = require('./includes.js');
 function deleteAndSendWarning(messageToDel,warning){
     messageToDel.delete()
     .then(()=>messageToDel.channel.send(warning))
-    .then((sentMessage)=>{
+    .then((warningSent)=>{
         setTimeout(()=>{
-            sentMessage.delete();
+            warningSent.delete();
         },16*1000)
+    })
+}
+
+/**
+ * 
+ * @param {string} url 
+ * @param {string} path 
+ * @param {string} filename
+ * @param {string | null} extension 
+ */
+function fetchToSave(url,folderPath){
+    fetch(url)
+    .catch(err=>console.log(err))
+    .then(res=>{
+        const dest=fse.createWriteStream(folderPath)
+        res.body.pipe(dest)
+        dest.on("error",err=>console.log(err))
     })
 }
 
@@ -35,9 +54,17 @@ module.exports=(message,client)=>{
         if(!exists)
         return deleteAndSendWarning(message,message.author.toString()+" Tu no estas participando, no puedes enviar cosas aquí")
 
+        //Revisa que no halla enviado algo que no hallan sido un archivo
         if(message.attachments.size==0 || message.attachments.some(attachment=>attachment.contentType.split("/")[0]!="image"))
         return deleteAndSendWarning(message,"Disculpa "+message.author.toString()+", este canal es solo para imagenes")
 
-        console.log(message.attachments.at(0))
+        /*
+        fetch(message.attachments.at(0).url)
+        .then(res=>{
+            res.body.pipe(fse.createWriteStream(path+"/semen.png"))
+        })
+        */
+
+        
     })
 }
